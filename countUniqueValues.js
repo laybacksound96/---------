fd714dc53d8cc9fdc493f4875597553023bec559
412 <laybacksound96@gmail.com>
2023-03-23 20:59:07 +0900

    countUniqueValues 해결

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -7,12 +7,17 @@ but in will always be sorted
 */
 
 function countUniqueValues(arrayParam) {
-  const aa = {};
+  var aa = {};
   for (var i = 0; i < arrayParam.length; i++) {
-    aa[i] ? (aa[i] += 1) : (aa = 0);
-    console.log(aa);
+    aa[arrayParam[i]] ? (aa[arrayParam[i]] += 1) : (aa[arrayParam[i]] = 1);
   }
+  return keys(aa).length;
 }
+
+// 1) 배열의 같은 숫자를 frequency couter로 모아보자
+// 1-1) [1,1,1,1,2] => {1:4 , 2:1}
+// 1-2) 배열의 인덱스를 순회해서 값이 있으면 1을 더하고 없으면 1을 할당하고
+// 2) 그 카운터의 길이가 고유값 개수 아닌가?
 
 countUniqueValues([1, 1, 1, 1, 1, 2]); // => 2
 countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // => 7
