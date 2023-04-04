da0719ed079823578057d3ede25804b9279a3a74
412 <laybacksound96@gmail.com>
2023-04-04 20:22:03 +0900

    04-04 recursion 풀이

--- fib.js
+++ fib.js
@@ -10,7 +10,11 @@ to the sum of the previous two numbers.
 // fib(10) // 55
 // fib(28) // 317811
 // fib(35) // 9227465
-
+/*
+파보나치 수열에서 n번째 숫자를 반환하는 재귀 함수인 fib를 작성하세요.
+파보나치 수열은 1, 1, 2, 3, 5, 8, ...과 같이 시작값이 1과 1이며,
+이후 모든 숫자가 이전 두 숫자의 합과 같은 수열입니다.
+*/
 function fib() {
   // add whatever parameters you deem necessary - good luck!
 }
